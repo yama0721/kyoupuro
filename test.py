48e7a298cc992bb3f5777fa3c3572800058a5eb2
yama0721 <nyanx678@gmail.com>
2021-07-02 14:18:12 +0900

    coins

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Hello World")
+a = int(input())
+b = int(input())
+c = int(input())
+x = int(input())
+
+
+
+for A in range(a):
+    for B in range(b):
+        for C in range(c):
+            total = 500*A + 100*B + 50*C
+            if total == X :
+                count += 1
+print(count)
